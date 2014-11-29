417042e3887799e821e5ea0ea4f3c576b953ffd5
Max Bucknell <me@maxbucknell.com>
2014-11-29 13:04:11 +0000

    Fix scoping issue.

--- test-runner.js
+++ test-runner.js
@@ -22,13 +22,13 @@ define([], function testRunnerMake () {
         console.log('Failed: ' + tests[i].desc)
         report += 'x'
       }
-
-      var total = report.length
-      var passed = report.replace('x', '').length
-
-      console.log(report)
-      console.log(passed + ' passed out of a total ' + total + ' taken.')
     }
+
+    var total = report.length
+    var passed = report.replace('x', '').length
+
+    console.log(report)
+    console.log(passed + ' passed out of a total ' + total + ' taken.')
   }
 
   return {
